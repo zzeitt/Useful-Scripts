f263efa7e2079b46b52784f8ced5be1e4eeeed33
zeit-lab <zei.t@qq.com>
2021-08-05 15:06:18 +0800

    update • AUC support

--- MATLAB/roc_curve.m
+++ MATLAB/roc_curve.m
@@ -23,16 +23,41 @@ function [] = my_main()
     zmax = 0.6;
     zstep = 0.01;
     s_savedir = './figures/';
+    fig_name = 'roc_0.0-0.6-0.01_isn_hips_udh_openstego_2';
+%     fig_name = 'None';
 
-    s_base = '/home/zeit/';
+    s_base = '/home/zeit/SDB/NiseEngFolder/newFile/forWork/forCooperation/forCVPR2021/ROC_csv/';
 
-    exp_hips = [s_base, 'for_roc_hips.csv'];
-    exp_isn = [s_base, 'for_roc_isn.csv'];
+%     exp_hips = [s_base, 'for_roc_hips.csv'];
+%     exp_isn = [s_base, 'for_roc_isn.csv'];
+%     exp_udh = [s_base, 'for_roc_udh.csv'];
+%     exp_openstego = [s_base, 'for_roc_openstego.csv'];
+    exp_hips = [s_base, 'hips.csv'];
+    exp_isn = [s_base, 'isn.csv'];
+    exp_udh = [s_base, 'udh.csv'];
+    exp_openstego = [s_base, 'openstego.csv'];
 
+    % Calculate positive rates.
     [x1, y1] = calc_pos_rate(exp_isn, zmin, zmax, zstep, 'cont', 'host');
     [x2, y2] = calc_pos_rate(exp_hips, zmin, zmax, zstep, 'cont', 'host');
+    [x3, y3] = calc_pos_rate(exp_udh, zmin, zmax, zstep, 'cont', 'host');
+    [x4, y4] = calc_pos_rate(exp_openstego, zmin, zmax, zstep, 'cont', 'host');
 
-    draw_roc([y1;y2], x2, 'roc_0.0-0.6-0.01_isn_hips', s_savedir, ["isn", "hips"]);
+    % Calculate area under roc curve (AUC).
+    auc_isn = trapz(fliplr(x1), fliplr(y1));
+    auc_hips = trapz(fliplr(x2), fliplr(y2));
+    auc_udh = trapz(fliplr(x3), fliplr(y3));
+    auc_openstego = trapz(fliplr(x4), fliplr(y4));
+    disp(['====> AUC ISN: ', string(auc_isn)]);
+    disp(['====> AUC HIPS: ', string(auc_hips)]);
+    disp(['====> AUC UDH: ', string(auc_udh)]);
+    disp(['====> AUC OpenStego: ', string(auc_openstego)]);
+    
+    % Draw ROC curve.
+    draw_roc(...
+        [y1;y2;y3;y4], [x1;x2;x3;x4], fig_name, s_savedir, ...
+        ["ISN", "HIPS", "UDH", "OpenStego"]);
+    
 end
 
 
@@ -58,7 +83,7 @@ end
 
 
 %% --------- Draw ROC curve ----------
-function [fig] = draw_roc(ys, x, save_img_name, s_savedir, li_names)
+function [fig] = draw_roc(ys, xs, save_img_name, s_savedir, li_names)
     sz = size(ys);
     num = sz(1);
     
@@ -71,17 +96,17 @@ function [fig] = draw_roc(ys, x, save_img_name, s_savedir, li_names)
         
     cc = hsv(num+1);
     for i = 1:num % Loop Y-values.
-        scatter(x, ys(i,:), 15, cc(i,:), 'filled', 'DisplayName', li_names(i));
+        scatter(xs(i,:), ys(i,:), 15, cc(i,:), 'filled', 'DisplayName', li_names(i));
         hold on;
-        plot(x, ys(i,:), 'Color', cc(i,:), 'LineStyle', '-', 'LineWidth', 1, 'HandleVisibility', 'off');
+        plot(xs(i,:), ys(i,:), 'Color', cc(i,:), 'LineStyle', '-', 'LineWidth', 1, 'HandleVisibility', 'off');
         hold on;
     end
     
     % Draw diagonal line.
-    x3 = linspace(0,1,30);
-    y3 = x3;
-    % plot(x3,y3,'Color', cc(end, :), 'LineWidth',1, 'HandleVisibility', 'on', 'DisplayName', 'diagonal'); % Use the 8th HSV color.
-    plot(x3,y3,'Color', cc(end, :), 'LineWidth',1, 'HandleVisibility', 'off');
+    x0 = linspace(0,1,30);
+    y3 = x0;
+    % plot(x0,y3,'Color', cc(end, :), 'LineWidth',1, 'HandleVisibility', 'on', 'DisplayName', 'diagonal'); % Use the 8th HSV color.
+    plot(x0,y3,'Color', cc(end, :), 'LineWidth',1, 'HandleVisibility', 'off');
     
     % Set legend.
     lgd = legend;
