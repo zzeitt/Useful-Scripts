6517454184648c4afba48344bdbd113e7176a465
zeit-lab <zei.t@qq.com>
2021-08-19 11:51:49 +0800

    update • turn off both monitors

--- Bash/turn_on_off_monitor.sh
+++ Bash/turn_on_off_monitor.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 BRT=`xrandr --verbose | grep -m 1 -i brightness | cut -f2 -d ' '`
+
 MNT="HDMI-0"
+echo "$MNT's brightness is $BRT"
+
+if [[ $BRT = "1.0" ]]
+then
+    echo "---> [-] Turning off the monitor..."
+    xrandr --output $MNT --brightness 0
+else
+    echo "---> [O] Turn on the monitor."
+    xrandr --output $MNT --brightness 1
+fi
+
+MNT="DP-3"
 echo "$MNT's brightness is $BRT"
 
 if [[ $BRT = "1.0" ]]
