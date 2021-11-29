32df4c9397d2458f40004b23c1d80f3da22c0ebb
zeit-lab <zei.t@qq.com>
2021-11-29 16:36:03 +0800

    update • conda export minimal docs

--- Bash/conda_export.sh
+++ Bash/conda_export.sh
@@ -3,3 +3,8 @@ conda env export > env.yml
 
 # Neatly export (but lose 'pip')
 conda env export --from-history > env.yml
+
+# Minimal export (include 'pip')
+pip install pyyaml
+pip install fire
+python Python/conda_export_minimal.py --s_save=env.yaml
