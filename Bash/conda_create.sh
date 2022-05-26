21a9459ed30ba427af54d783fd680a3c5aaf4db8
zeit <zei.t@qq.com>
2022-05-26 15:57:53 +0800

    update • minor changes

--- Bash/conda_create.sh
+++ Bash/conda_create.sh
@@ -1,13 +1,8 @@
 # From scratch
 conda create -p ./env python=3.9
 
-<<<<<<< HEAD
-# From definitino file
+# From definition file
 conda env create -f env.yml -p ./env
 
 # Install pip packages
 conda env update -f pkgs.yml -p ./env
-=======
-# From definition file
-conda env create -f env.yml -p ./env
->>>>>>> fa69fd50a39033681a5d07591335ad2b175a4095
