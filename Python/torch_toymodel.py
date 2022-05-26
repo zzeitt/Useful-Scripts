39d509eef61fba8a8b12f8af2768439f26cb28aa
zeit <zei.t@qq.com>
2022-05-26 15:54:29 +0800

    add • toy model

---
+++ Python/torch_toymodel.py
@@ -0,0 +1,9 @@
+class ToyModel(nn.Module):
+    def __init__(self):
+        super(ToyModel, self).__init__()
+        self.net1 = nn.Linear(10, 10)
+        self.relu = nn.ReLU()
+        self.net2 = nn.Linear(10, 5)
+
+    def forward(self, x):
+        return self.net2(self.relu(self.net1(x)))
